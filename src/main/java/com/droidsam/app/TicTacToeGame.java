3ed29b912fc6140143d29bc52c36824579b7f42b
droidsam1 <droidsam@gmail.com>
2022-10-30 10:37:37 +0100

    refactor: adds semantics by creating method enforce player move rules

--- src/main/java/com/droidsam/app/TicTacToeGame.java
+++ src/main/java/com/droidsam/app/TicTacToeGame.java
@@ -17,20 +17,35 @@ public class TicTacToeGame {
     }
 
     public void place(Player player, int x, int y) {
-        if (marks == 0 && Player.X != player) {
-            throw new InvalidParameterException("Player X always goes first");
-        }
-        if (player == lasPlayer) {
-            throw new InvalidParameterException("Players alternate placing marks on the board");
-        }
-        if (squares[x][y] != null) {
-            throw new InvalidParameterException("Can not place over squares that has already been played");
-        }
+        enforcePlayerMoveRules(player, x, y);
 
         lasPlayer = player;
         squares[x][y] = player;
 
         marks++;
+    }
 
+    private void enforcePlayerMoveRules(Player player, int x, int y) {
+        playerXAlwaysStartTheGame(player);
+        playersMustAlternate(player);
+        squaresCanNotPlayedAgain(x, y);
+    }
+
+    private void squaresCanNotPlayedAgain(int x, int y) {
+        if (squares[x][y] != null) {
+            throw new InvalidParameterException("Can not place over squares that has already been played");
+        }
+    }
+
+    private void playersMustAlternate(Player player) {
+        if (player == lasPlayer) {
+            throw new InvalidParameterException("Players alternate placing marks on the board");
+        }
+    }
+
+    private void playerXAlwaysStartTheGame(Player player) {
+        if (marks == 0 && Player.X != player) {
+            throw new InvalidParameterException("Player X always goes first");
+        }
     }
 }
