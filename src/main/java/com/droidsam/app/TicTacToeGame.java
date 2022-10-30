467b510b56d4cd92d5ce0423025af78eb2fe70fc
droidsam1 <droidsam@gmail.com>
2022-10-30 11:05:29 +0100

    refactor: remove squares responsability from game class

--- src/main/java/com/droidsam/app/TicTacToeGame.java
+++ src/main/java/com/droidsam/app/TicTacToeGame.java
@@ -1,17 +1,13 @@
 package com.droidsam.app;
 
 import java.security.InvalidParameterException;
-import java.util.Arrays;
-import java.util.Objects;
 
 public class TicTacToeGame {
 
-    final PlayerMark[][] squares;
     final Board board;
     private PlayerMark lasPlayer;
 
     public TicTacToeGame() {
-        squares = new PlayerMark[3][3];
         board = new Board();
     }
 
@@ -23,20 +19,12 @@ public class TicTacToeGame {
         enforcePlayerMoveRules(player, x, y);
 
         lasPlayer = player;
-        squares[x][y] = player;
         board.place(player, x, y);
     }
 
     private void enforcePlayerMoveRules(PlayerMark player, int x, int y) {
         playerXAlwaysStartTheGame(player);
         playersMustAlternate(player);
-        squaresCanNotPlayedAgain(x, y);
-    }
-
-    private void squaresCanNotPlayedAgain(int x, int y) {
-        if (squares[x][y] != null) {
-            throw new InvalidParameterException("Can not place over squares that has already been played");
-        }
     }
 
     private void playersMustAlternate(PlayerMark player) {
