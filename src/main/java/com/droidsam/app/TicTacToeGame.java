66370fb1f27e30b780802aa1c05655561cebe78e
droidsam1 <droidsam@gmail.com>
2022-10-30 10:05:03 +0100

    add game starts with player x

--- src/main/java/com/droidsam/app/TicTacToeGame.java
+++ src/main/java/com/droidsam/app/TicTacToeGame.java
@@ -1,7 +1,15 @@
 package com.droidsam.app;
 
 public class TicTacToeGame {
+
+    int marks = 0;
+
     public boolean isGridEmpty() {
-        return true;
+        return marks == 0;
+    }
+
+    public void place(char player, int x, int y) {
+        marks++;
+
     }
 }
--- src/test/java/com/droidsam/app/TicTacToeGameTest.java
+++ src/test/java/com/droidsam/app/TicTacToeGameTest.java
@@ -2,6 +2,7 @@ package com.droidsam.app;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TicTacToeGameTest {
@@ -9,6 +10,13 @@ public class TicTacToeGameTest {
     @Test
     public void shouldStartWithEmptyGrid() {
         assertTrue(new TicTacToeGame().isGridEmpty());
+    }
+
+    @Test
+    public void shouldPlayerXStartTheGame() {
+        var game = new TicTacToeGame();
+        game.place('X', 1, 1);
+        assertFalse(game.isGridEmpty());
     }
 
 }
